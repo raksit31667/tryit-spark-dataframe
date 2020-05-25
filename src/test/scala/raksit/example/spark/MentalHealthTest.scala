245b59be367a53f72e1a6b0555e2e2eafa26a061
Raksit <rmanta1@ap.xom.com>
2020-05-25 22:16:38 +0700

    :white_check_mark: add test for dataframe equality using DataFrameSuiteBase

--- src/test/scala/raksit/example/spark/MentalHealthTest.scala
+++ src/test/scala/raksit/example/spark/MentalHealthTest.scala
@@ -1,10 +1,11 @@
 package raksit.example.spark
 
-import com.holdenkarau.spark.testing.SharedSparkContext
-import org.apache.spark.sql.DataFrame
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row}
 import org.scalatest.FunSuite
 
-class MentalHealthTest extends FunSuite with SharedSparkContext {
+class MentalHealthTest extends FunSuite with DataFrameSuiteBase {
 
   test("should return 170 yes when getTreatmentByGender given gender is female") {
     // When
@@ -64,5 +65,16 @@ class MentalHealthTest extends FunSuite with SharedSparkContext {
     val actual = actualDataFrame.filter(row => row.getAs[String]("Gender").equals("Transgender"))
       .select("No").collect().toSeq
     assert(actual.head.get(0) === 4)
+  }
+
+  test("should return dataframe with gender and total yes-no counts when getTreatmentByGender") {
+    // When
+    val actualDataFrame: DataFrame = MentalHealth.getTreatmentByGender
+
+    // Then
+    val expectedSchema: StructType = StructType(List(StructField("Gender", StringType), StructField("Yes", LongType), StructField("No", LongType)))
+    val expectedData: Seq[Row] = Seq(Row("Female", 170L, 77L), Row("Male", 450L, 541L), Row("Transgender", 17L, 4L))
+    val expectedDataFrame: DataFrame = spark.createDataFrame(sc.parallelize(expectedData), expectedSchema)
+    assertDataFrameNoOrderEquals(expectedDataFrame, actualDataFrame)
   }
 }
