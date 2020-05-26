7752d8cd776167a37ebf015aa5218063f9ed6896
Raksit <raksit.m@ku.th>
2020-05-26 23:15:45 +0700

    :white_check_mark: add test case in NestedJsonExpander for partially expanded json

--- src/test/scala/raksit/example/spark/NestedJsonExpanderTest.scala
+++ src/test/scala/raksit/example/spark/NestedJsonExpanderTest.scala
@@ -31,6 +31,31 @@ class NestedJsonExpanderTest extends FunSuite with DataFrameSuiteBase {
     assertDataFrameNoOrderEquals(expectedDataFrame, actualDataFrame)
   }
 
+  test("should return expanded dataframe when getDataFrameFromNestedJson given partially expanded json path") {
+    // When
+    val actualDataFrame: DataFrame = NestedJsonExpander.getDataFrameFromNestedJson("/partially_expanded.json")
+
+    // Then
+    val expectedSchema: StructType = StructType(List(
+      StructField("CHECK_Check1", LongType),
+      StructField("CHECK_Check2", StringType),
+      StructField("COL", LongType),
+      StructField("DATA_Crate_key", StringType),
+      StructField("DATA_Crate_value", StringType),
+      StructField("DATA_MLrate", StringType),
+      StructField("IFAM", StringType),
+      StructField("KTM", LongType)
+    ))
+    val expectedData: Seq[Row] = Seq(
+      Row(1L, "TWO", 21L, "k1", "v1", "31", "EQR", 1548176931466L),
+      Row(1L, "TWO", 21L, "k2", "v2", "31", "EQR", 1548176931466L),
+      Row(1L, "TWO", 21L, "k3", "v3", "33", "EQR", 1548176931466L),
+      Row(1L, "TWO", 21L, "k4", "v4", "33", "EQR", 1548176931466L)
+    )
+    val expectedDataFrame: DataFrame = spark.createDataFrame(sc.parallelize(expectedData), expectedSchema)
+    assertDataFrameNoOrderEquals(expectedDataFrame, actualDataFrame)
+  }
+
   test("should return same dataframe when getDataFrameFromNestedJson given already expanded json path") {
     // When
     val actualDataFrame: DataFrame = NestedJsonExpander.getDataFrameFromNestedJson("/already_expanded.json")
