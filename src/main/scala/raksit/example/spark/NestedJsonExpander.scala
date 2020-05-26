f7288dc312eff01a417968edd1e59dc95037c8e8
Raksit <raksit.m@ku.th>
2020-05-26 17:13:59 +0700

    :recycle: inline private method for expandNestedJson to reduce loops

--- src/main/scala/raksit/example/spark/NestedJsonExpander.scala
+++ src/main/scala/raksit/example/spark/NestedJsonExpander.scala
@@ -1,6 +1,6 @@
 package raksit.example.spark
 
-import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, explode}
 import org.apache.spark.sql.types.{ArrayType, StructType}
 
@@ -10,49 +10,37 @@ object NestedJsonExpander extends InitSpark {
 
   def getDataFrameFromNestedJson(jsonPath: String): DataFrame = {
     var nestedJsonDataFrame = sqlContext.read.json(
-      sparkContext.parallelize(Source.fromInputStream(getClass.getResourceAsStream(jsonPath)).getLines().mkString :: Nil)
-    )
+      sparkContext.parallelize(
+        Source.fromInputStream(getClass.getResourceAsStream(jsonPath)).getLines().mkString :: Nil))
     nestedJsonDataFrame.show(truncate = false)
 
-    var nestedColumns = 1
+    var isStillNested: Boolean = true
 
-    while (nestedColumns != 0) {
-      var tempNestedColumns = 0
+    while (isStillNested) {
+      isStillNested = false
+      var tempDataFrame: DataFrame = nestedJsonDataFrame
+      var selectClauseList: List[String] = List.empty[String]
 
-      for (columnName <- nestedJsonDataFrame.schema.fieldNames) {
-        nestedJsonDataFrame.schema(columnName).dataType match {
-          case _: ArrayType | _: StructType => tempNestedColumns += 1
-          case _ => tempNestedColumns += 0
+      for (columnName <- tempDataFrame.schema.fieldNames) {
+        tempDataFrame.schema(columnName).dataType match {
+          case _: ArrayType =>
+            isStillNested = true
+            tempDataFrame = tempDataFrame.withColumn(columnName, explode(col(columnName)))
+            selectClauseList = selectClauseList :+ columnName
+          case _: StructType =>
+            isStillNested = true
+            for (fieldName <- tempDataFrame.schema(columnName).dataType.asInstanceOf[StructType].fieldNames) {
+              selectClauseList = selectClauseList :+ s"$columnName.$fieldName"
+            }
+          case _ => selectClauseList = selectClauseList :+ columnName
         }
       }
 
-      if (tempNestedColumns != 0) {
-        nestedJsonDataFrame = expandNestedColumn(nestedJsonDataFrame)
-        nestedJsonDataFrame.show(truncate = false)
-      }
-      nestedColumns = tempNestedColumns
+      nestedJsonDataFrame = tempDataFrame.select(
+        selectClauseList.map(name => col(name).alias(name.replace('.', '_'))): _*)
+      nestedJsonDataFrame.show(truncate = false)
     }
 
     nestedJsonDataFrame
-  }
-
-  private def expandNestedColumn(dataFrame: DataFrame): DataFrame = {
-    var tempDataFrame: DataFrame = dataFrame
-    var selectClauseList: List[String] = List.empty[String]
-
-    for (columnName <- tempDataFrame.schema.fieldNames) {
-      tempDataFrame.schema(columnName).dataType match {
-        case _: ArrayType =>
-          tempDataFrame = tempDataFrame.withColumn(columnName, explode(col(columnName)))
-          selectClauseList = selectClauseList :+ columnName
-        case _: StructType =>
-          for (fieldName <- tempDataFrame.schema(columnName).dataType.asInstanceOf[StructType].fieldNames) {
-            selectClauseList = selectClauseList :+ s"$columnName.$fieldName"
-          }
-        case _ => selectClauseList = selectClauseList :+ columnName
-      }
-    }
-    val columns: List[Column] = selectClauseList.map(name => col(name).alias(name.replace('.', '_')))
-    tempDataFrame.select(columns: _*)
   }
 }
