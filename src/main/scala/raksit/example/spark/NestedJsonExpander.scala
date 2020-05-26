cac2754ca710804f3c12fab3a39a9ac1c0f4d414
Raksit <raksit.m@ku.th>
2020-05-26 23:08:10 +0700

    :recycle: use tail recursion to refactor getDataFrameFromNestedJson

--- src/main/scala/raksit/example/spark/NestedJsonExpander.scala
+++ src/main/scala/raksit/example/spark/NestedJsonExpander.scala
@@ -4,43 +4,45 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, explode}
 import org.apache.spark.sql.types.{ArrayType, StructType}
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object NestedJsonExpander extends InitSpark {
 
   def getDataFrameFromNestedJson(jsonPath: String): DataFrame = {
-    var nestedJsonDataFrame = sqlContext.read.json(
+    val nestedJsonDataFrame = sqlContext.read.json(
       sparkContext.parallelize(
         Source.fromInputStream(getClass.getResourceAsStream(jsonPath)).getLines().mkString :: Nil))
     nestedJsonDataFrame.show(truncate = false)
+    expandNestedDataFrame(nestedJsonDataFrame)
+  }
 
-    var isStillNested: Boolean = true
+  @tailrec
+  private def expandNestedDataFrame(nestedDataFrame: DataFrame): DataFrame = {
+    var tempDataFrame = nestedDataFrame
+    var selectClauseList: List[String] = List.empty[String]
 
-    while (isStillNested) {
-      isStillNested = false
-      var tempDataFrame: DataFrame = nestedJsonDataFrame
-      var selectClauseList: List[String] = List.empty[String]
-
-      for (columnName <- tempDataFrame.schema.fieldNames) {
-        tempDataFrame.schema(columnName).dataType match {
-          case _: ArrayType =>
-            isStillNested = true
-            tempDataFrame = tempDataFrame.withColumn(columnName, explode(col(columnName)))
-            selectClauseList = selectClauseList :+ columnName
-          case _: StructType =>
-            isStillNested = true
-            for (fieldName <- tempDataFrame.schema(columnName).dataType.asInstanceOf[StructType].fieldNames) {
-              selectClauseList = selectClauseList :+ s"$columnName.$fieldName"
-            }
-          case _ => selectClauseList = selectClauseList :+ columnName
-        }
+    for (columnName <- tempDataFrame.schema.fieldNames) {
+      tempDataFrame.schema(columnName).dataType match {
+        case _: ArrayType =>
+          tempDataFrame = tempDataFrame.withColumn(columnName, explode(col(columnName)))
+          selectClauseList = selectClauseList :+ columnName
+        case _: StructType =>
+          for (fieldName <- tempDataFrame.schema(columnName).dataType.asInstanceOf[StructType].fieldNames) {
+            selectClauseList = selectClauseList :+ s"$columnName.$fieldName"
+          }
+        case _ => selectClauseList = selectClauseList :+ columnName
       }
-
-      nestedJsonDataFrame = tempDataFrame.select(
-        selectClauseList.map(name => col(name).alias(name.replace('.', '_'))): _*)
-      nestedJsonDataFrame.show(truncate = false)
     }
 
-    nestedJsonDataFrame
+    val expandedDataFrame = tempDataFrame.select(
+      selectClauseList.map(name => col(name).alias(name.replace('.', '_'))): _*)
+    expandedDataFrame.show(truncate = false)
+
+    if (expandedDataFrame.schema.equals(nestedDataFrame.schema) && expandedDataFrame.except(nestedDataFrame).count() == 0) {
+      return expandedDataFrame
+    }
+
+    expandNestedDataFrame(expandedDataFrame)
   }
 }
--- src/test/scala/raksit/example/spark/NestedJsonExpanderTest.scala
+++ src/test/scala/raksit/example/spark/NestedJsonExpanderTest.scala
@@ -6,9 +6,34 @@ import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType
 import org.scalatest.FunSuite
 
 class NestedJsonExpanderTest extends FunSuite with DataFrameSuiteBase {
-  test("should return dataframe when getDataFrameFromNestedJson given nested json path") {
+  test("should return expanded dataframe when getDataFrameFromNestedJson given nested json path") {
     // When
     val actualDataFrame: DataFrame = NestedJsonExpander.getDataFrameFromNestedJson("/nested.json")
+
+    // Then
+    val expectedSchema: StructType = StructType(List(
+      StructField("CHECK_Check1", LongType),
+      StructField("CHECK_Check2", StringType),
+      StructField("COL", LongType),
+      StructField("DATA_Crate_key", StringType),
+      StructField("DATA_Crate_value", StringType),
+      StructField("DATA_MLrate", StringType),
+      StructField("IFAM", StringType),
+      StructField("KTM", LongType)
+    ))
+    val expectedData: Seq[Row] = Seq(
+      Row(1L, "TWO", 21L, "k1", "v1", "31", "EQR", 1548176931466L),
+      Row(1L, "TWO", 21L, "k2", "v2", "31", "EQR", 1548176931466L),
+      Row(1L, "TWO", 21L, "k3", "v3", "33", "EQR", 1548176931466L),
+      Row(1L, "TWO", 21L, "k4", "v4", "33", "EQR", 1548176931466L)
+    )
+    val expectedDataFrame: DataFrame = spark.createDataFrame(sc.parallelize(expectedData), expectedSchema)
+    assertDataFrameNoOrderEquals(expectedDataFrame, actualDataFrame)
+  }
+
+  test("should return same dataframe when getDataFrameFromNestedJson given already expanded json path") {
+    // When
+    val actualDataFrame: DataFrame = NestedJsonExpander.getDataFrameFromNestedJson("/already_expanded.json")
 
     // Then
     val expectedSchema: StructType = StructType(List(
