e67557bf769049ea9ab44c66b6d25f58b98870e5
Raksit <raksit.m@ku.th>
2020-06-06 17:06:43 +0700

    🔊 add log about Kafka offset in WordCountStreaming

--- src/main/scala/raksit/example/spark/kafka/WordCountStreamingApp.scala
+++ src/main/scala/raksit/example/spark/kafka/WordCountStreamingApp.scala
@@ -2,11 +2,12 @@ package raksit.example.spark.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.TaskContext
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, HasOffsetRanges, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import raksit.example.spark.InitSpark
 
-object WordCountStreaming extends InitSpark {
+object WordCountStreamingApp extends InitSpark {
   def main(args: Array[String]): Unit = {
     val streamingContext = new StreamingContext(sparkContext, Seconds(10))
     val topics = Set("wordcount")
@@ -16,17 +17,27 @@ object WordCountStreaming extends InitSpark {
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
       ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
     )
+
     val messages = KafkaUtils.createDirectStream[String, String](
       streamingContext,
       LocationStrategies.PreferConsistent,
       ConsumerStrategies.Subscribe[String, String](topics, kafkaParameters)
     )
+
+    messages.foreachRDD { rdd =>
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd.foreachPartition { _ =>
+        val offsetRange = offsetRanges(TaskContext.getPartitionId())
+        println(s"Kafka info: fromOffset=${offsetRange.fromOffset}, untilOffset=${offsetRange.untilOffset}")
+      }
+    }
+
     val text = messages.map(_.value())
     text.print() // Hello world
     val words = text.flatMap(_.split(" "))
     val wordCounts = words.map(x => (x, 1L)).reduceByKey(_+_)
 
-    wordCounts.print() //(Hello,1) (world,1)
+    wordCounts.print() // (Hello,1) (world,1)
 
     streamingContext.start()
     streamingContext.awaitTermination()
