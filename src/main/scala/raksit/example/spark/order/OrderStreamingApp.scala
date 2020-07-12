79041ff7485d8a5a14e9e80f018b47e9224f4b9a
Raksit <raksit.m@ku.th>
2020-07-12 18:54:32 +0700

    ♻️ leverage high-order functions on Order streaming

--- src/main/scala/raksit/example/spark/order/OrderStreamingApp.scala
+++ src/main/scala/raksit/example/spark/order/OrderStreamingApp.scala
@@ -1,8 +1,9 @@
 package raksit.example.spark.order
 
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StringType, StructField, StructType}
 import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import raksit.example.spark.InitSpark
 import raksit.example.spark.order.OrderAggregator.collectOrdersByClientId
 import raksit.example.spark.order.OrderIdValidator.addValidOrderIdFlag
@@ -13,38 +14,43 @@ object OrderStreamingApp extends InitSpark {
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-    val inputStream = spark.readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "order")
-      .load()
-
-    val inputJsonString = inputStream.selectExpr("CAST (value AS STRING)").as[String]
-
-    val orderSchema = StructType(
-      List(StructField("orderId", LongType),
-        StructField("clientId", StringType),
-        StructField("amount", IntegerType),
-        StructField("price", DoubleType)))
-
-    val orderDataFrame = inputJsonString.select(from_json(col("value"), orderSchema))
-
-    val aggregatedOrderDataFrame: DataFrame = processOrder(orderDataFrame)
-
-    val streamingQuery = aggregatedOrderDataFrame
-      .selectExpr("CAST(clientId AS STRING) AS key", "to_json(struct(*)) AS value")
-      .writeStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("topic", "order_aggregated")
-      .start()
-
-    streamingQuery.awaitTermination()
+    initSpark() andThen fromKafka() andThen processOrder andThen sendBackToKafka()
   }
 
   def processOrder(orderDataFrame: DataFrame): DataFrame = {
     addValidOrderIdFlag() andThen
       addValidPriceAndAmountFlag() andThen
       collectOrdersByClientId() apply orderDataFrame
+  }
+
+  private def initSpark(): Void => SparkSession = {
+    _ => spark
+  }
+
+  private def fromKafka(): SparkSession => DataFrame = {
+    val orderSchema = StructType(
+      List(StructField("orderId", LongType),
+        StructField("clientId", StringType),
+        StructField("amount", IntegerType),
+        StructField("price", DoubleType)))
+
+    spark =>
+      spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "order")
+        .load()
+        .selectExpr("CAST (value AS STRING)").as[String]
+        .select(from_json(col("value"), orderSchema))
+  }
+
+  private def sendBackToKafka(): DataFrame => StreamingQuery = {
+    dataFrame =>
+      dataFrame.selectExpr("CAST(clientId AS STRING) AS key", "to_json(struct(*)) AS value")
+        .writeStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("topic", "order_aggregated")
+        .start()
   }
 }
