14429625e61ab0c619c168ee7d6ea8f3bfdb854e
Raksit <raksit.m@ku.th>
2020-07-16 22:23:38 +0700

    ♻️ move kafka-related commands to newly-created class for Order streaming

---
+++ src/main/scala/raksit/example/spark/order/KafkaStreamingManager.scala
@@ -0,0 +1,32 @@
+package raksit.example.spark.order
+
+import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object KafkaStreamingManager {
+
+  def fromKafka(schema: StructType): SparkSession => DataFrame = {
+    spark =>
+      import spark.implicits._
+
+      spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "order")
+        .load()
+        .selectExpr("CAST (value AS STRING)").as[String]
+        .select(from_json(col("value"), schema))
+  }
+
+  def sendBackToKafka(topic: String): DataFrame => StreamingQuery = {
+    dataFrame =>
+      dataFrame.selectExpr("CAST(clientId AS STRING) AS key", "to_json(struct(*)) AS value")
+        .writeStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("topic", topic)
+        .start()
+  }
+}
--- src/main/scala/raksit/example/spark/order/OrderStreamingApp.scala
+++ src/main/scala/raksit/example/spark/order/OrderStreamingApp.scala
@@ -1,20 +1,20 @@
 package raksit.example.spark.order
 
-import org.apache.spark.sql.functions.{col, from_json}
-import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, SparkSession}
 import raksit.example.spark.InitSpark
+import raksit.example.spark.order.KafkaStreamingManager.{fromKafka, sendBackToKafka}
 import raksit.example.spark.order.OrderAggregator.collectOrdersByClientId
 import raksit.example.spark.order.OrderIdValidator.addValidOrderIdFlag
 import raksit.example.spark.order.OrderQuantitativeValidator.addValidPriceAndAmountFlag
 
 object OrderStreamingApp extends InitSpark {
 
-  import spark.implicits._
-
   def main(args: Array[String]): Unit = {
-    initSession() andThen fromKafka() andThen processOrder andThen sendBackToKafka()
+    initSession() andThen
+      fromKafka(getOrderSchema) andThen
+      processOrder andThen
+      sendBackToKafka("order_aggregated")
   }
 
   def processOrder(orderDataFrame: DataFrame): DataFrame = {
@@ -23,30 +23,11 @@ object OrderStreamingApp extends InitSpark {
       collectOrdersByClientId() apply orderDataFrame
   }
 
-  private def fromKafka(): SparkSession => DataFrame = {
-    val orderSchema = StructType(
+  private def getOrderSchema = {
+    StructType(
       List(StructField("orderId", LongType),
         StructField("clientId", StringType),
         StructField("amount", IntegerType),
         StructField("price", DoubleType)))
-
-    spark =>
-      spark.readStream
-        .format("kafka")
-        .option("kafka.bootstrap.servers", "localhost:9092")
-        .option("subscribe", "order")
-        .load()
-        .selectExpr("CAST (value AS STRING)").as[String]
-        .select(from_json(col("value"), orderSchema))
-  }
-
-  private def sendBackToKafka(): DataFrame => StreamingQuery = {
-    dataFrame =>
-      dataFrame.selectExpr("CAST(clientId AS STRING) AS key", "to_json(struct(*)) AS value")
-        .writeStream
-        .format("kafka")
-        .option("kafka.bootstrap.servers", "localhost:9092")
-        .option("topic", "order_aggregated")
-        .start()
   }
 }
